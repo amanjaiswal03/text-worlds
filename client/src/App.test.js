1248330d939a67ea895821ec2f3dcbe0a6755e5a
Aman Jaiswal <amanjaiswal@Aman-Jaiswal.local>
2020-04-14 16:47:16 +0200

    snapshot testing done

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import {render, cleanup} from '@testing-library/react'
+import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+// cleanup after each test to avoid memory leaks 
+afterEach(cleanup)
+ 
+//testing to ensure fragment of the App component matches the snapshot
+ it('should take a snapshot', () => {
+    const { asFragment } = render(<App />)
+    
+    expect(asFragment(<App />)).toMatchSnapshot()
+  });
